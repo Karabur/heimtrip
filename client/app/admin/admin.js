b6c434ca6cff477a5e0cbe205d8fb0e9acc5b929
Anton Ulyanov <anton.ulianov@gmail.com>
2014-11-26 23:39:22 +0300

    Initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('heimtripApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
